76fe9d69c38b57f0224573744277e1517e397571
Hong Huang <39689053+Geoffrey1500@users.noreply.github.com>
2021-04-18 17:45:35 +0800

    尝试重投影

--- calcultor.py
+++ calcultor.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
 
-rotation_matrix = np.zeros(shape=(3,3))
-cv2.Rodrigues((1, 0, 0), rotation_matrix)
-#Apply rotation matrix to point
-original_point = np.array([[1],[0],[0]])
-rotated_point = rotation_matrix*original_point
-print(rotated_point)
+
+rotation_vector = np.array([[-0.07760355, -0.02770259, -0.01145906]]).T
+print(rotation_vector, rotation_vector.shape)
+rotation_matrix, jacobian = cv2.Rodrigues(rotation_vector)
+print(rotation_matrix)
+
+tvecs = np.array([[100, 0, 0]]).T
+
+R_and_T = np.vstack((np.hstack((rotation_matrix, tvecs)), np.array([[0, 0, 0, 1]])))
+print(R_and_T)
---
+++ find_pose_trial_2.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+with np.load('sony_16mm.npz') as X:
+    mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((11*8, 3), np.float32)
+objp[:, :2] = np.mgrid[0:11, 0:8].T.reshape(-1, 2)*30
+
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[1].ravel())
+    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    return img
+
+
+axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)*30
+print(axis)
+
+
+for fname in glob.glob('imgs/pose_test_2/*.jpg'):
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, (11, 8), None)
+    if ret == True:
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        # Find the rotation and translation vectors.
+        ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
+
+        rotation_matrix, jacobian = cv.Rodrigues(rvecs)
+        R_and_T = np.vstack((np.hstack((rotation_matrix, tvecs)), np.array([[0, 0, 0, 1]])))
+
+        projection_mode = np.array([[1, 0, 0, 0],
+                                    [0, 1, 0, 0],
+                                    [0, 0, 1, 0]])
+
+        # img_cor =
+
+        # project 3D points to image plane
+        imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        img = draw(img, corners2, imgpts)
+        cv.namedWindow("img", 0)
+        cv.resizeWindow("img", 1080, 720)
+        cv.imshow('img', img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
